eb8f48e799e1660ea2a0b5a624e8e3b4836cd4fa
RenadShamrani <s443000808@st.uqu.edu.sa>
2024-05-16 14:47:00 +0000

    Renad

--- functions.sh
+++ functions.sh
@@ -44,7 +44,7 @@ checkout() {
         return
     fi
     echo "-------------------------------"
-    echo "** Electronics Store - Receipt**"
+    echo "** Electronics Direct - Receipt**"
     total_price=0
     for product in "${selected_products[@]}"; do
         price=$(echo "${prices[$product]}" | awk '{print int($1 + rand() * ($2 - $1 + 1))}')
@@ -55,8 +55,30 @@ checkout() {
     echo "Total Price: \$$total_price"
     read -p "How would you like to pay? (cash/credit): " payment_method
     payment_method=$(echo "$payment_method" | tr '[:upper:]' '[:lower:]')
-    if [ "$payment_method" == "cash" ] || [ "$payment_method" == "credit" ]; then
-        echo "Thank you for your purchase! Your $payment_method payment has been processed."
+    if [ "$payment_method" == "cash" ]; then
+        echo "Thank you for your purchase! Your cash payment has been processed."
+        elif [ "$payment_method" == "credit" ]; then
+        while true; do
+            read -p "Enter the name on the card: " card_name
+            read -p "Enter the card number (16 digits): " card_number
+            read -p "Enter the expiry date (MM/YY): " expiry_date
+            read -p "Enter the CVS (3 digits): " cvs
+            
+            if [[ ${#card_number} -ne 16 ]]; then
+                echo "Invalid card number. It must be exactly 16 digits."
+                continue
+                elif [[ ${#expiry_date} -ne 5 ]]; then
+                echo "Invalid expiry date. It must be exactly 5 characters (MM/YY)."
+                continue
+                elif [[ ${#cvs} -ne 3 ]]; then
+                echo "Invalid CVS. It must be exactly 3 digits."
+                continue
+            else
+                break
+            fi
+        done
+        echo "Thank you for your purchase! Your credit card payment has been processed."
+        
     else
         echo "Invalid payment method. Please choose cash or credit."
     fi
